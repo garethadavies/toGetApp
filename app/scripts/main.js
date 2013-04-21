50a109b5e237d19a420e98da3f9dff20367b716c
Gareth Davies <garethaneurindavies@gmail.com>
2013-04-21 17:06:43 +0100

    Added topbar and list code and icon fonts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,16 +23,39 @@ require.config({
         'backbone'
       ],
       exports : 'Marionette'
-    },
+    }
   },
   paths: {
-    jquery: '../components/zepto/zepto',
-    backbone: '../components/backbone-amd/backbone',
-    underscore: '../components/underscore-amd/underscore',
-    marionette: '../components/marionette/lib/core/amd/backbone.marionette'
+    jquery: 'vendor/jquery/jquery',
+    backbone: 'vendor/backbone-amd/backbone',
+    underscore: 'vendor/underscore-amd/underscore',
+    marionette: 'vendor/marionette/lib/core/amd/backbone.marionette',
+    'backbone.wreqr' : 'vendor/marionette/public/javascripts/backbone.wreqr',
+    'backbone.babysitter' : 'vendor/marionette/public/javascripts/backbone.babysitter'
   }
 });
 
-require(['backbone', 'marionette'], function (Backbone) {
-  Backbone.history.start();
+require(['backbone', 'marionette'], function (Backbone, Marionette) {
+
+  // set up the app instance
+  var App = new Marionette.Application();
+
+  App.addRegions({
+    
+    someRegion: '#some-div',
+    anotherRegion: '#another-div'
+  
+  });
+
+  App.addInitializer(function(options) {
+
+  // new MyAppRouter();
+  
+    Backbone.history.start();
+
+  });
+
+  // export the app from this module
+  return App;
+
 });
