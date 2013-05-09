d995b3aeb92781035dd891e7348963116f0f4910
Gareth Davies <garethaneurindavies@gmail.com>
2013-05-09 21:13:53 +0100

    Added remove button to items

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ define(['marionette', 'backbone', 'snap'], function (Marionette, Backbone) {
     var snapper = new Snap({
 
       element: document.getElementById('content')
-      
+
     });
 
     $('#open-left').on('click', function() {
@@ -38,60 +38,19 @@ define(['marionette', 'backbone', 'snap'], function (Marionette, Backbone) {
 
     });
 
-    // console.log('hello');
+    $('.open-left').on('click', function() {
 
-    // $('.left-menu').sidr();
+      snapper.open('right');
 
-    // $('.right-menu').sidr({
+    });
 
-    //   name: 'sidr2',
-    //   side: 'right'
+    $('.close-panels').on('click', function() {
 
-    // });
+      snapper.close();
 
-    // $('.close-sidr2').on('click', function() {
+    });
 
-    //   $.sidr('close', 'sidr2');
-
-    // });
-
-    // $('.close-sidr').on('click', function() {
-
-    //   $.sidr('close', 'sidr');
-
-    // });
-
-    // var hammertime = Hammer(element, {
-    //     drag_min_distance: 20
-    // });
-
-    // var openLeft = Hammer('#main-list').on('swiperight', function(event) {
-
-    //   $.sidr('close', 'sidr2');
-    //   $.sidr('open', 'sidr');
-
-    // });
-
-    // var openRight = Hammer('#main-list').on('swipeleft', function(event) {
-
-    //   $.sidr('close', 'sidr');
-    //   $.sidr('open', 'sidr2');
-
-    // });
-
-    // var closeLeft = Hammer('#sidr').on('swipeleft', function(event) {
-
-    //   $.sidr('close', 'sidr');
-    //   // $.sidr('open', 'sidr');
-
-    // });
-
-    // var closeRight = Hammer('#sidr2').on('swiperight', function(event) {
-
-    //   $.sidr('close', 'sidr2');
-    //   // $.sidr('open', 'sidr2');
-
-    // });
+    
 
   });
 
